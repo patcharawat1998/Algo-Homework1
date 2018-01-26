793147abc334c3692362023147aab2db45bc4e21
patcharawat1998 <patcharawat.1998@gmail.com>
2018-01-26 22:02:16 +0700

    test1
    test1

---
+++ Algo-Homework1/src/Homework1.java
@@ -0,0 +1,31 @@
+
+public class Homework1 {
+
+	public static void main(String[] args) {
+//		// Begin of arguments input sample
+//		if (args.length > 0) {
+//			String input = args[0];
+//			if (input.equalsIgnoreCase("251-*32*+")) {
+//				System.out.println("(2*(5-1))+(3*2)=14");
+//			}
+//		}
+//		// End of arguments input sample
+//		
+//		// TODO: Implement your project here
+		
+		String input = "251-*32*+";
+		input = "1251-*32*+*";
+        Medhod medhod = new Medhod();
+
+        if (args.length > 0) {
+            input = args[0];
+        }
+
+        medhod.makeTree(input);
+        medhod.inorder();
+        medhod.infix();
+        medhod.printResult();
+
+		
+	}
+}
